5edd89236b95d8d9a5095e98121c2d29211ef8a9
Micahel Naberezhnui <mnaber2017@ukr.net>
2017-11-06 21:55:09 +0200

    fiz

--- js/preload.js
+++ js/preload.js
@@ -3,10 +3,8 @@ var n=0;
 var video;
  var w=0;
 var step;
-window.onload=done;
-
-function done(){
-  video = document.getElementsByTagName('video');
+window.addEventListener("load",function(){
+	video = document.getElementsByTagName('video');
   step=500/video.length;
 
    for(i=0;i<video.length;i++)
@@ -14,19 +12,19 @@ function done(){
 	   w+=step;
        videoDone();
    }
-}
-
+});
+ 
 function videoDone()
 { n+=1;
   var stat=(100/video.length)*n;
   document.getElementById('status').innerHTML ='Loading '+stat+" %";
   document.getElementById('status_bar').style.width=w+'px';
-   setInterval(function(){ if(n==video.length){
-		document.getElementById("preloader").style.opacity='0';
+      if(n==video.length)
+		  { setTimeout(function(){document.getElementById("preloader").style.opacity='0';
 		document.getElementById("preloader").style.zIndex='0';
-	    document.getElementById('status_bar').style.display= none;
-		stat.innerHTML='';
-	}},700);
-
+	    document.getElementById('status_bar').style.display= "none";
+		stat.innerHTML='';},700);
+		
+		  }
 }
 
